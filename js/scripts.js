5b158cc9e5668ef7813135133c46489648488aa0
VN <zmuda@nahodil.org>
2026-03-01 16:37:00 +0100

    small fixes

--- js/scripts.js
+++ js/scripts.js
@@ -120,7 +120,7 @@ $(document).ready(function () {
 
     $(function () {
 
-        $('a[href*=#]:not([href=#])').click(function () {
+        $('a[href*=#]:not([href=#]):not([data-toggle])').click(function () {
             if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
                 var target = $(this.hash);
