a500269ea7d52194cc46dbbf78060342ff02e5c5
VN <zmuda@nahodil.org>
2026-03-01 16:03:58 +0100

    minor styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -127,7 +127,7 @@ $(document).ready(function () {
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     $('html,body').animate({
-                        scrollTop: target.offset().top - 90
+                        scrollTop: target.offset().top - 120
                     }, 2000);
                     return false;
                 }
