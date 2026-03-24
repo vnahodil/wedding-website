e6a8a1e79c68ae715149b399dc733b3b61c63d62
VN <zmuda@nahodil.org>
2026-03-24 16:53:51 +0100

    český kalendář

--- js/vendor/ouical.js
+++ js/vendor/ouical.js
@@ -27,7 +27,7 @@
                 '&sprop=&sprop=name:'
             ].join(''));
             return '<a class="icon-google" target="_blank" href="' +
-                href + '">Google Calendar</a>';
+                href + '">Google Kalendář</a>';
         },
 
         yahoo: function(event) {
@@ -82,7 +82,7 @@
                     'END:VCALENDAR'].join('\n'));
 
             return '<a class="' + eClass + '" target="_blank" href="' +
-                href + '">' + calendarName + ' Calendar</a>';
+                href + '">' + calendarName + ' Kalendář</a>';
         },
 
         ical: function(event) {
@@ -97,7 +97,6 @@
     var generateCalendars = function(event) {
         return {
             google: calendarGenerators.google(event),
-            yahoo: calendarGenerators.yahoo(event),
             ical: calendarGenerators.ical(event),
             outlook: calendarGenerators.outlook(event)
         };
@@ -129,7 +128,7 @@
         var result = document.createElement('div');
 
         result.innerHTML = '<label id="add-to-calendar-label" for="checkbox-for-' +
-            calendarId + '" class="btn btn-fill btn-small"><i class="fa fa-calendar"></i>&nbsp;&nbsp; Add to Calendar</label>';
+            calendarId + '" class="btn btn-fill btn-small"><i class="fa fa-calendar"></i>&nbsp;&nbsp; Přidat do kalendáře</label>';
         result.innerHTML += '<input name="add-to-calendar-checkbox" class="add-to-calendar-checkbox" id="checkbox-for-' + calendarId + '" type="checkbox">';
 
         Object.keys(calendars).forEach(function(services) {
