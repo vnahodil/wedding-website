51802782c4f902856978128430c81e451e0cc6f9
kolihjar <jardak.18@seznam.cz>
2026-04-02 00:14:22 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -187,7 +187,7 @@ $(document).ready(function () {
         $('#alert-wrapper').html(alert_markup('info', '<strong>Vteřinku!</strong> Ukládáme vaše údaje.'));
 
         if (MD5($('#invite_code').val().toLowerCase()) !== '4bd2b007716888ed6bf6c2399a6d7305') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Promiň!</strong> Tvůj kód z pozvánky je nesprávný.'));
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Promiň!</strong> Zadej celé křestní jméno.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbx_Ak4Yo8X3gls1Tj3KadQPSJpeOKCr1W9691oGiC6kZF9NECsOpzqscoZiTGbKErMdlA/exec', data)
                 .done(function (data) {
